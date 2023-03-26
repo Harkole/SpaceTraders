9019afbd3b8ade22138230e104d676b7f4f63bc7
Will Blackburn <harkole@gmail.com>
2023-03-26 21:12:37 +0100

    Created flightplan DTO

---
+++ SpaceTraders.Ui/Models/FlightPlanDto.cs
@@ -0,0 +1,27 @@
+﻿namespace SpaceTraders.Ui.Models
+{
+    public class FlightPlanDto
+    {
+        public DateTime ArrivesAt { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string? Departure { get; set; }
+
+        public string? Destination { get; set; }
+
+        public int Distance { get; set; }
+
+        public int FuelConsumed { get; set; }
+
+        public int FuelRemaining { get; set; }
+
+        public string? Id { get; set; }
+
+        public string? ShipId { get; set; }
+
+        public string? TerminatedAt { get; set; }
+
+        public int TimeRemainingInSeconds { get; set; }
+    }
+}
