5c39367d2453ef3911898796a52b89c7d2efd453
Will Blackburn <harkole@gmail.com>
2023-03-26 18:33:31 +0100

    Created DTO objects for player loans

---
+++ SpaceTraders.Ui/Models/LoanDto.cs
@@ -0,0 +1,30 @@
+﻿namespace SpaceTraders.Ui.Models
+{
+    public class LoanDto
+    {
+        /// <summary>
+        /// When the loan is due to be repaid
+        /// </summary>
+        public DateTime Due { get; set; }
+
+        /// <summary>
+        /// The unique identity of the loan
+        /// </summary>
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// The amount required to repay the loan
+        /// </summary>
+        public int RepaymentAmount { get; set; }
+
+        /// <summary>
+        /// The current loan status
+        /// </summary>
+        public string? Status { get; set; }
+
+        /// <summary>
+        /// The type of loan
+        /// </summary>
+        public string? Type { get; set; }
+    }
+}
---
+++ SpaceTraders.Ui/Models/LoansDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SpaceTraders.Ui.Models
+{
+    public class LoansDto
+    {
+        public IEnumerable<LoanDto> Loans { get; set; }
+    }
+}
